1cd568ec0b8e339c0a9b08a3d13d5baabb36ce32
Alexander Ivanov <oshli.a.er@gmail.com>
2019-05-15 11:07:08 +0300

    Format adn fix the issue
    Signed-off-by: Alexander Ivanov <oshli.a.er@gmail.com>

--- src/Код.js
+++ src/Код.js
@@ -1,34 +1,35 @@
+/* exported doGet, include, completedWithoutErrors, doneWithAnError, addContent, getId */
+
 function doGet() {
   var template = HtmlService.createTemplateFromFile('top');
   return template.evaluate();
 }
-//генрерирует $main.js на клиенте
+// генрерирует $main.js на клиенте
 function include(filename) {
-  return HtmlService.createHtmlOutputFromFile(filename)
-  .getContent();
+  return HtmlService.createHtmlOutputFromFile(filename).getContent();
 }
 // в случае успешного выполнения при нажатии на кнопку на клиенте
-function completedWithoutErrors(e){
-  Logger.log('Успешное выполнение. Сервер :'+e)
+function completedWithoutErrors(e) {
+  Logger.log('Успешное выполнение. Сервер :' + e);
 }
 // в случае ошибки при нажатии на кнопку на клиенте
-function doneWithAnError(e){
-  Logger.log('Выполнено с ошибкой. Сообщение об ошибке :'+e)
+function doneWithAnError(e) {
+  Logger.log('Выполнено с ошибкой. Сообщение об ошибке :' + e);
 }
 // возвращает контент листа confirmed на клиент
-function addContent (filename){
+function addContent(filename) {
   var content = HtmlService.createHtmlOutputFromFile(filename).getContent();
-   return content
+  return content;
 }
-//возврацает на клиент имя листа confirmed для div id="content"
-function getId(){
-   var id_content= addSrt();
-   Logger.log(id_content)
-   return id_content
+// возврацает на клиент имя листа confirmed для div id="content"
+function getId() {
+  var idContent = addSrt();
+  Logger.log(idContent);
+  return idContent;
 }
 
 // просто возварщает имя листа
-function addSrt(){
-      var str = 'confirmed';
-   return str
-  }
+function addSrt() {
+  var str = 'confirmed';
+  return str;
+}
